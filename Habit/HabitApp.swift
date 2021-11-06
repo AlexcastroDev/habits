e03837cbbe10c2990fc4a26b1f5db2ec80af44fa
Alexandro <alexoliveira7x@gmail.com>
2021-11-06 17:04:58 +0000

    Initial Commit

---
+++ Habit/HabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitApp.swift
+//  Habit
+//
+//  Created by Alexandro castro on 06/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct HabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
